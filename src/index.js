a232d5cfdcaa3149642ed20f56082f61c2245619
wadha08 <wadha-ra@hotmail.com>
2019-09-24 22:29:31 +0300

    everything is working now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./stores/reducer";
+
 import registerServiceWorker from "./registerServiceWorker";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
